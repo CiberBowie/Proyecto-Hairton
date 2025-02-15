8bbfcfa0ab62373be2287fd716d759e63b6da9a0
CiberBowie <hairtonchalcolima@gmail.com>
2025-02-15 03:30:30 -0300

    Cambio y mejoras para futuras versiones en calculo de deltas

--- src/core/coordinate_transformer.py
+++ src/core/coordinate_transformer.py
@@ -1,11 +1,15 @@
 from pyproj import Transformer
-
+"""
+Codigo necesario para el cambio de formato entre ciudades y aplicacciones, en futuras versiones tendra soporte para varios formatos
+*Recordatorio:crear un diccionario con diferentes formatos
+"""
 class CoordinateTransformer:
     def __init__(self, delta_x = 0, delta_y = 0):
+        #Creamos el objeto Transformer y le decimos que queremos que transforme de un tipo a otro 
         self.transformer = Transformer.from_crs("EPSG:4326", "EPSG:3735", always_xy=True)
         self.delta_x = 0
         self.delta_y = 0
-    
+        #modifica los atributos del objeto, sin embargo puede que esto cambie en futuras versiones
     def set_deltas(self, delta_x, delta_y):
         self.delta_x = delta_x
         self.delta_y = delta_y
